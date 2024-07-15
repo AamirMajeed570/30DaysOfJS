a7b16138e021ccbd84ea647ce1410d21cdce0702
Aamir Majeed Khan <aamirmajeedkhan570@gmail.com>
2024-07-15 13:20:32 +0530

    Day1 and day2 Completed JS

---
+++ Day01/index.js
@@ -0,0 +1,39 @@
+// var num = 10;
+// console.log(num);
+// let str = "aamir";
+// console.log(str);
+// const boolean = true;
+// console.log(boolean);
+
+// let num = 10;
+// let str = "aamir";
+// let bool = false;
+// const obj = {
+//     name: "aamir"
+// }
+// let arr = [1,2,3,4,5]; // Array is of type Object
+
+// console.log(typeof num)
+// console.log(typeof str)
+// console.log(typeof bool)
+// console.log(typeof obj)
+// console.log(typeof arr)
+
+// Reassigning Variables 
+// let num= 10;
+// console.log(num);
+// num = 12;
+// console.log(num);
+
+// const num = 10;
+// console.log(num);
+// num = 12; /// Error
+// console.log(num);
+
+const variableTypes = (num) =>{
+    console.log(num);
+    console.log(typeof num);
+}
+
+let arr = 10;
+variableTypes(arr);
---
+++ Day02/index.js
@@ -0,0 +1,57 @@
+// const add = (num1,num2)=>{
+//     return num1 + num2;
+// }
+
+// const subtraction = (num1,num2) =>{
+//     return num1-num2;
+// }
+
+// const multiply = (num1,num2) =>{
+//     return num1*num2;
+// }
+
+// const divide = (num1,num2) =>{
+//     if(num2===0){
+//         return 'Cannot Divide By Zero';
+//     }
+//     else{
+//         return num1/num2;
+//     }
+// }
+
+// console.log(add(10,20));
+// console.log(subtraction(20,10));
+// console.log(multiply(20,10));
+// console.log(divide(20,10));
+
+// const assignmentVariable = (num) =>{
+//     return num += 10;
+// }
+
+// const deassignemntVariable = (num) =>{
+//     return num -= 10;
+// }
+
+// console.log(assignmentVariable(20));
+// console.log(deassignemntVariable(20));
+
+// const compareVariables = (num1,num2) =>{
+//     if(num1>num2){
+//         return num1;
+//     }
+//     else{
+//         return num2;
+//     }
+// }
+// console.log("Greatest Number is ",compareVariables(9,10))
+
+// const compareVariables = (num1,num2) =>{
+//     if(num1<=num2)
+//         return num1;
+//     else
+//         return num2;
+// }
+
+// console.log('Greatest Variable is ', compareVariables(30,10));
+let num = -10;
+console.log(num<0?'Number is Negative':'Number is Positive')
