78ed001ae9cf2e7327afa691baafe91258f5ca88
Aamir Majeed Khan <aamirmajeedkhan570@gmail.com>
2024-07-16 09:42:36 +0530

    Day04 Task Completed

---
+++ Day04/index.js
@@ -0,0 +1,46 @@
+// for(let i=0;i<=10;i++){
+//     console.log(i," ");
+// }
+
+// for(let i=1;i<=10;i++){
+//     console.log("5 "+" * ",i," ",5*i);
+// }
+
+// let i = 1;
+// let sum = 0;
+// while(i<10){
+//     sum += i;
+//     i++;
+// }
+// console.log(sum);
+
+// let i = 1;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<=5);
+
+// Factorial Of a Number
+// let fact = 1;
+// let i = 5;
+// do{
+//     fact = fact * i;
+//     i--;
+// }while(i!=0)
+
+//     console.log(fact);
+//  for (let index = 0; index < 10; index++) {
+//     if(index==5)
+//         continue;
+//     else
+//         console.log(index);
+    
+//  }
+
+ for (let index = 0; index < 10; index++) {
+    if(index==7)
+        break;
+    else
+        console.log(index);
+    
+ }
